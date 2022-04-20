5fab6245905f4b1710d19ec6c2b4d2b95548d509
Alan <azzheng@umass.edu>
2022-04-20 18:48:27 -0400

    made some changes

--- textbooks.js
+++ textbooks.js
@@ -2,20 +2,6 @@ import * as http from 'http';
 import * as url from 'url';
 import { readFile, writeFile, access } from 'fs/promises';
 
-const buy = document.getElementById("buy-option");
-const sell = document.getElementById("sell-option");
-const listlabel = document.getElementById("input-label");
-const title = document.getElementById("input-title");
-const subtitle = document.getElementById("input-subtitle");
-const isbn = document.getElementById("input-isbn");
-const author = document.getElementById("input-author");
-const edition = document.getElementById("input-edition");
-const trade = document.getElementById("btn-to-trade");
-const lsell = document.getElementById("btn-to-sell");
-const sellamount = document.getElementById("input-sell-amt");
-const imageUpload = document.getElementById("image-upload");
-
-const button = document.getElementsId("btn-add-listing");
 const JSONfile = "textbook.json";
 
 let library = {};
@@ -41,16 +27,6 @@ async function update() {
 
 export async function storeBook(response) {
     await load();
-    let book = {};
-    book["option"] = buy.value == ""? sell.value: buy.value;
-    book["listlabel"] = listlabel.value; 
-    book["title"] = title.value;
-    book["subtitle"] = subtitle.value;
-    book["author"] = author.value;
-    book["edition"] = edition.value;
-    book["transaction"] = trade.value == ""? sellamount.value: trade.value;
-    // sets varaible to an int if trade is empty else return string
-    library[isbn.value] = book;
     update();
     response.writeHead(200, { 'Content-Type': 'application/json'});
     response.write(JSON.stringify(library));
