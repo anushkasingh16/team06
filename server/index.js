91b7cb3bc6dd2ed4e9c9dfd64906f2376dd5bfff
Anushka Singh <argentanushka@icloud.com>
2022-04-20 21:55:46 -0400

    HEROKU steps + debugging + changed express routes

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
